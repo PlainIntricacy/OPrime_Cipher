e860e7cdf26df638aa584dfec3d5bb0cd101379a
Tudor Laptes <tudorlaptes@gmail.com>
2014-10-13 14:19:40 -0400

    Initial commit

---
+++ src/oprime_cipher/OPrime_Cipher.java
@@ -0,0 +1,121 @@
+/**
+ * 
+ * Java program that simulates a circular primes encryption device.
+ * Unlike a regular Caesar cipher, this version uses the key as its circular prime upper limit.
+ * This limit is then used to calculate the sum of positive circular primes below it.
+ * The calculated sum is then used as the shift number to encrypt the message.
+ * 
+ * This a safer version of the Caesar cipher, since it offers two layers of security:
+ * Either the key or the method can be made public without revealing the message.
+ * 
+ * Based on problem 35 on projecteuler.net, and the Caesar Cipher.
+ * 
+ * https://projecteuler.net/problem=35
+ * https://en.wikipedia.org/wiki/Caesar_cipher
+ * https://github.com/PlainIntricacy/Circular_Primes
+ * 
+ */
+package oprime_cipher;
+
+import java.util.Scanner;
+
+/*
+ *  @author todyerutz @ plainintricacy.wordpress.com
+ */
+public class OPrime_Cipher {
+
+    public static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Please enter your message:");
+            String inText = input.nextLine();
+            char[] arr = inText.toUpperCase().toCharArray();
+            System.out.println("Please enter your key:");
+            int sKey = Integer.parseInt(input.nextLine());
+            System.out.println("Encrypted message:");
+            encrypt(arr, sumOPrimes(sKey));
+            System.out.println();
+            input.close();
+        }
+    }
+    
+    public static boolean isLetter(Character x){
+        for(int i=0; i<alphabet.length(); i++){
+            if(x==alphabet.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static int Lpos(Character x){
+        for(int i=0; i<alphabet.length(); i++){
+            if(x==alphabet.charAt(i)){
+                return i;
+            }
+        }
+        return 0;
+    }
+    
+    public static int Apos(int x){
+        if(x>alphabet.length()){
+            return x-alphabet.length();
+        }else
+            if(x<0){
+                return alphabet.length()+x;
+            }        
+        return x;
+    }
+            
+    public static void encrypt(char[] a, int x){
+        for(int i=0; i<a.length; i++){
+            if(isLetter(a[i])){
+                a[i] = alphabet.charAt(Lpos(alphabet.charAt(Apos(Lpos(a[i])+x))));
+            }
+        }
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i]);
+        }
+    }
+    
+    public static int sumOPrimes(int x){
+        int sum=0;
+        if(x>2){
+            sum+=2;
+        }
+        for(int i=3; i<=x; i+=2){
+           int temp = i; 
+           if(primality(i)&&primality(rotate(temp))){
+               sum+=i;
+           }           
+        }
+        return sum;
+    }
+    
+    public static boolean primality(int q){
+        if(q==2){
+            return true;
+        }else
+        if(q%2==0&&q!=2){
+            return false;
+        }else{
+        for(int i=3; i<=Math.sqrt(q); i+=2){
+                if(q%i==0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public static int rotate(int x){
+        int y=0;
+        do{
+            y*=10;
+            y+=x%10;
+            x=x/10;
+        }while(x>0);
+        return y;
+    }
+}
